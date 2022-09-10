4284ad02ed8e8916d74de9a0e9f830ca352590c5
JannisErhard <jannis.erhard@gmx.de>
2022-09-10 12:50:05 +0200

    git rid of some debug output

--- FCI.py
+++ FCI.py
@@ -26,16 +26,8 @@ print(pyscf.__version__)
 
 
 myhf = tools.fcidump.to_scf('fcidump', molpro_orbsym=False, mf=None)
-print(f"myhf.mo_occ")
 myhf.run()
 
-
-DEBUG = False
-
-if DEBUG:
-    dm = myhf.make_rdm1()
-    print("This is the HF DM")
-    print(dm)
 
 # create an FCI solver based on the SCF object
 cisolver = pyscf.fci.FCI(myhf)
@@ -43,36 +35,16 @@ cisolver.conv_tol = 1e-9
 
 
 e, fcivec = cisolver.kernel(verbose=5)
-print(f"E={e}")
 
 
 norb = myhf.mo_coeff.shape[1]
-
-if DEBUG:
-    # show contents of scf object
-    for element in dir(myhf):
-        print(element, getattr(myhf,element))
-
-if DEBUG:
-    for element in dir(myhf.mol):
-        print(element, getattr(myhf.mol, element))
 
 # 6 alpha electrons, 4 beta electrons because spin = nelec_a-nelec_b = 2
 dm1 = cisolver.make_rdm1(fcivec, myhf.mo_coeff.shape[0], myhf.mol.nelec)
 orbitals = read_orbitals('Test_File')
 dm_molpro = np.matmul(np.matmul(orbitals,dm1), orbitals.transpose())
 
-DEBUG = False  
-if DEBUG:
-    maybe = cisolver.__dict__['mol']
-    for element in dir(maybe):
-        print(element, getattr(maybe,element))
-
-if DEBUG:
-    print("Density Matrix From PySCF")
-    print(dm1)
-    print("Density Matrix From Molpro")
-    print(dm_molpro)
+print(f"E={e}")
 
 write_matrop('dm.txt',dm_molpro, norb)
 
